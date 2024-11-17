c56ceed306c0c062244071074fc0ebfaef352549
arianamesquita <arianamesquita85@gmail.com>
2024-11-17 15:48:18 -0300

    feat(forum): tornando o componente dinamico na criação de várias respostas

--- src/app/external/views/components/forum/forum.component.ts
+++ src/app/external/views/components/forum/forum.component.ts
@@ -19,6 +19,30 @@ export class ForumComponent {
   form: FormGroup;
   mostrarRespostas = false;
 
+  perguntas = [
+    {
+      usuario: "Usuário X",
+      periodo: "Período 6",
+      data: "11/12/2020",
+      pergunta: "Question? Question? Question?",
+      descricao: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.",
+      respostas: [
+        {
+          usuario: "Usuário Y",
+          periodo: "Período YY",
+          data: "12/12/2020",
+          resposta: "Resposta 1 para a pergunta."
+        },
+        {
+          usuario: "Usuário Z",
+          periodo: "Período ZZ",
+          data: "13/12/2020",
+          resposta: "Resposta 2 para a pergunta."
+        }
+      ]
+    }
+  ];
+
   constructor(
     private readonly builder: FormBuilder,
     private readonly service: ExternalService ){
