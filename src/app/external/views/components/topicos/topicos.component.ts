15925b9eb38925bb6223070d79bd5e6d8838e559
arianamesquita <arianamesquita85@gmail.com>
2024-11-18 09:26:01 -0300

    feature(topicos): tela abertura topicos finalizada

--- src/app/external/views/components/topicos/topicos.component.ts
+++ src/app/external/views/components/topicos/topicos.component.ts
@@ -10,11 +10,22 @@ export class TopicosComponent {
 
   form: FormGroup;
 
+  categorias = [
+    {nome: "Redes", icon : '../../../../../assets/icons/icon-redes.svg'},
+    {nome: "Banco de Dados", icon : '../../../../../assets/icons/icon-banco-dados.svg'},
+    {nome: "Programação", icon : '../../../../../assets/icons/icon-programacao.svg'},
+    {nome: "Web", icon : '../../../../../assets/icons/icon-web.svg'},
+    {nome: "Estrutura de Dados", icon : '../../../../../assets/icons/icon-estrutura-dados.svg'},
+    {nome: "Arquitetura de Computadores", icon : '../../../../../assets/icons/icon-arquitetura-comp.svg'},
+  ]
+
   constructor(
     private readonly builder: FormBuilder ){
     this.form = this.builder.group({
       pesquisa: [null,]
     })
+    console.log(this.categorias)
+    
   }
   
 }
