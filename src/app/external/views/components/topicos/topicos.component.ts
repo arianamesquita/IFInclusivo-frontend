aae9ff36fbd54d4737723ef0d580e9dd5e27e632
arianamesquita <arianamesquita85@gmail.com>
2024-11-18 22:02:40 -0300

    feature(topicos): implementação inicial página pesquisa

--- src/app/external/views/components/topicos/topicos.component.ts
+++ src/app/external/views/components/topicos/topicos.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TopicosComponent {
 
   form: FormGroup;
+  start: boolean;
+  second: boolean;
 
   categorias = [
     {nome: "Redes", icon : '../../../../../assets/icons/icon-redes.svg'},
@@ -19,13 +21,27 @@ export class TopicosComponent {
     {nome: "Arquitetura de Computadores", icon : '../../../../../assets/icons/icon-arquitetura-comp.svg'},
   ]
 
+  mockData = [
+    { nome: "HTML", descricao: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web..."},
+    { nome: "HTML", descricao: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web..."},
+    { nome: "HTML", descricao: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web..."},
+    { nome: "HTML", descricao: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web..."},
+    { nome: "HTML", descricao: "HTML é uma linguagem de marcação utilizada na construção de páginas na Web..."},
+  ]
+
   constructor(
     private readonly builder: FormBuilder ){
     this.form = this.builder.group({
       pesquisa: [null,]
     })
-    console.log(this.categorias)
-    
+    this.start = true;
+    this.second = false;
+  }
+
+  changePage(event: any){
+    console.log(this.mockData)
+    this.start = false;
+    this.second = true;
   }
   
 }
